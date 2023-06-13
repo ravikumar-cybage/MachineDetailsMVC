1a94ec471d54e5747621b1f42f5fa995d95f2b50
Ravi Kumar <ravkumar@cybage.com>
2023-06-13 11:04:51 +0530

    Add project files.

---
+++ MachineDetailsMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MachineDetailsMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class HomeController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public HomeController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+        _httpClient.BaseAddress =
+            new Uri("https://localhost:7138/api/");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7138/api/Machine");
+       if (response != null)
+        {
+            var resultString = await response.Content.ReadAsStringAsync();
+            dynamic machineDetails = JsonConvert.DeserializeObject(resultString);
+            ViewBag.MachineName = machineDetails.machineName;
+            ViewBag.MachineIP = machineDetails.machineIP;
+            ViewBag.MachineOS = machineDetails.machineOS;
+            ViewBag.Timestamp = machineDetails.timestamp;
+        }
+
+        return View();
+    }
+}
