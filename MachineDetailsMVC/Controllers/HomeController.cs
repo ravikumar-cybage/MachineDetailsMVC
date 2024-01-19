8daa8fa45548c5551debac65307aa12d542899ad
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2024-01-19 16:39:03 +0530

    Update HomeController.cs

--- MachineDetailsMVC/Controllers/HomeController.cs
+++ MachineDetailsMVC/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ public class HomeController : Controller
     {
         _httpClient = httpClientFactory.CreateClient();
         _httpClient.BaseAddress =
-            new Uri("http://172.17.0.3:80/api/Machine");
+            new Uri("http://172.17.0.2:80/api/Machine");
  
     }
 
